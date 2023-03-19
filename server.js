d42b12abc473b4c7454146463928aeb52e221cbd
Tony Trinh <tonytrinh911@gmail.com>
2023-03-18 20:09:09 -0400

    added routes for HTML and API; need to work on API post route

--- server.js
+++ server.js
@@ -9,3 +9,18 @@ app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use(express.static('public'));
 
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+} )
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+})
+
+app.get('/api/notes', (req, res) => {
+    res.send(JSON.stringify(notes));
+})
+
+app.post('/api/notes', (req, res) => {
+    
+})
